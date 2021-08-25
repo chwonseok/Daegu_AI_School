72560eee3a5dc07c3cce495454678b1c24707567
cws0316 <dev.chwonseok@gmail.com>
2021-08-25 17:08:01 +0900

    Added few more things

--- 18-javascript-practice/index.js
+++ 18-javascript-practice/index.js
@@ -1,5 +1,13 @@
 // null and undefined
+// console.log(null + 10)
+// console.log(null + undefined)
 
 // function
+function nameFunc(lastName, firstName) {
+  return lastName + '' + firstName;
+}
 
+const fullName = nameFunc;
 // array
+const arr = [1, 2, 3, 4, [5, 6, 7, [8, 9]]];
+// object
