3bd5dd2f1c73a7b7cae946f670c5ef41e9bcbced
cws0316 <dev.chwonseok@gmail.com>
2021-08-26 01:22:10 +0900

    Done second JS lecture

--- 18-javascript-practice/index.js
+++ 18-javascript-practice/index.js
@@ -8,6 +8,26 @@ function nameFunc(lastName, firstName) {
 }
 
 const fullName = nameFunc;
+
 // array
 const arr = [1, 2, 3, 4, [5, 6, 7, [8, 9]]];
+
 // object
+const student = {
+  firstName: 'Wonseok',
+  lastName: 'Choi',
+  age: 32,
+  skills: ['html', 'css', 'JS', 'react'],
+  sum(a, b) {
+    return a + b;
+  },
+};
+
+// Primitive type vs Reference type
+// let str1 = 'a';
+// let str2 = 'b';
+// str2 = str1;
+// str1 = 'c';
+// console.log(str1, str2);
+
+//
