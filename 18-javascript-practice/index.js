0d1f025cb21abac0040e8221085c1768e880f4c0
cws0316 <dev.chwonseok@gmail.com>
2021-08-27 15:05:49 +0900

    Studies if, function, hoisting etc.

--- 18-javascript-practice/index.js
+++ 18-javascript-practice/index.js
@@ -3,25 +3,25 @@
 // console.log(null + undefined)
 
 // function
-function nameFunc(lastName, firstName) {
-  return lastName + '' + firstName;
-}
+// function nameFunc(lastName, firstName) {
+//   return lastName + '' + firstName;
+// }
 
-const fullName = nameFunc;
+// const fullName = nameFunc;
 
 // array
-const arr = [1, 2, 3, 4, [5, 6, 7, [8, 9]]];
+// const arr = [1, 2, 3, 4, [5, 6, 7, [8, 9]]];
 
 // object
-const student = {
-  firstName: 'Wonseok',
-  lastName: 'Choi',
-  age: 32,
-  skills: ['html', 'css', 'JS', 'react'],
-  sum(a, b) {
-    return a + b;
-  },
-};
+// const student = {
+//   firstName: 'Wonseok',
+//   lastName: 'Choi',
+//   age: 32,
+//   skills: ['html', 'css', 'JS', 'react'],
+//   sum(a, b) {
+//     return a + b;
+//   },
+// };
 
 // Primitive type vs Reference type
 // let str1 = 'a';
@@ -30,4 +30,48 @@ const student = {
 // str1 = 'c';
 // console.log(str1, str2);
 
-//
+//////////////////////////////////////////////////////////////
+// if, else if, else, ternary operator
+// parseInt():
+// 전역변수, 지역변수
+// const globalV = 'global';
+// if (true) {
+//   const localV = 'local';
+
+//   console.log(globalV);
+//   console.log(localV);
+// }
+
+// console.log(globalV);
+// console.log(localV);
+// function a() {
+//   const apple = 'apple';
+// }
+
+// function b() {
+//   a();
+//   console.log(apple);
+// }
+// b();
+
+// lexical scoping
+// let name = 'wonseok';
+// function func1() {
+//   console.log(name);
+// }
+// function func2() {
+//   let name = 'choi';
+//   func1();
+// }
+// func2();
+
+// Hoisting
+// 변수 호이스팅, 함수 호이스팅
+// test1();
+// function test1() {
+//   console.log('this is test1');
+// }
+// test2();
+// const test2 = function () {
+//   console.log('this is test2');
+// };
