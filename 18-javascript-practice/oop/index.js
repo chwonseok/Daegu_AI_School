cdf0e51b74dee7293397ba22b5eedc0365230989
cws0316 <dev.chwonseok@gmail.com>
2021-09-02 14:43:28 +0900

    Done with Typewriter App

--- 18-javascript-practice/oop/index.js
+++ 18-javascript-practice/oop/index.js
@@ -12,14 +12,72 @@
 // myself.fullName('ya');
 
 //// Prototype ************************************
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+// function Person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
+
+// Person.prototype.fullName = function (hey) {
+//   console.log(`${this.firstName} ${this.lastName} ${hey}`);
+// };
+
+// const myself = new Person('wonseok', 'choi');
+// myself.fullName('hey');
+
+// const arr = [10, 20, 30, 40];
+// let index = 0;
+
+// function tips() {
+//   const current = index % arr.length;
+//   // console.log(current);
+//   console.log(arr[current]);
+//   index++;
+
+//   setTimeout(() => {
+//     tips();
+//   }, 1000);
+// }
+
+// tips();
+const txtEl = document.querySelector('.txt');
+const bikes = ['bianchi', 'trek', 'canyon', 's-works'];
+
+function Typewriter(txtElement, bikes) {
+  this.txtElement = txtElement;
+  this.bikes = bikes;
+
+  this.txt = '';
+  this.bikeIndex = 0;
+  this.isDeleting = false;
+
+  this.type();
 }
 
-Person.prototype.fullName = function (hey) {
-  console.log(`${this.firstName} ${this.lastName} ${hey}`);
+Typewriter.prototype.type = function () {
+  const current = this.bikeIndex % this.bikes.length;
+  const fullTxt = this.bikes[current];
+
+  // 한 글자씩 줄어드는 이펙트
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  }
+
+  // 한 글자씩 늘어나는 이펙트
+  if (!this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  this.txtElement.textContent = this.txt;
+
+  // 글자 완성 되었을 때, 글자가 모두 삭제되었을 때
+  if (!this.isDeleting && this.txt === fullTxt) this.isDeleting = true;
+  if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.bikeIndex++;
+  }
+
+  setTimeout(() => {
+    this.type();
+  }, 200);
 };
 
-const myself = new Person('wonseok', 'choi');
-myself.fullName('hey');
+new Typewriter(txtEl, bikes);
